419fc55b6427548badb9281123bbf92c46891939
Hunter Scholz <scholzh@oregonstate.edu>
2026-02-03 17:28:40 -0800

    project 4 fixed

--- Projects/project4/redirout.c
+++ Projects/project4/redirout.c
@@ -5,17 +5,19 @@
 #include <fcntl.h>
 
 int main (int argc, char *argv[]){
-    if (argc <= 3){
+    if (argc < 3){
         printf("usage: redirout filename command [arg1 [arg2 ... ] ]\n");
         exit(1);
     }
 
     char *fout = argv[1];
-    int fd = open(fout, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+    int fd = open(fout, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR, 0644);
+    // Unsure which permissions to put here, hope this works
+
     if (fd == -1) {
         perror("open");
         exit(1);
-    } 
+    }
 
     dup2(fd, 1);
 
