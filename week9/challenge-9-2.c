a7790d40524663e6d675d8c295017f7b4be335bf
Hunter Scholz <scholzh@oregonstate.edu>
2026-03-02 11:30:06 -0800

    challenge 9-2 complete

---
+++ week9/challenge-9-2.c
@@ -0,0 +1,64 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
+
+int count = 0;
+
+void lock(void){
+    pthread_mutex_lock(&mutex);
+}
+
+void unlock(void){
+    pthread_mutex_unlock(&mutex);
+}
+
+void *run1(void *arg) {
+    (void)arg;
+
+    while (count != 3){
+        lock();
+        pthread_cond_wait(&condition, &mutex);
+        unlock();
+    }
+
+    puts("Halfway there!");
+    return NULL;
+}
+
+void *run2(void *arg){
+    (void)arg;
+
+    while (count != 0){
+        lock();
+        pthread_cond_wait(&condition, &mutex);
+        unlock();
+    }
+    
+    puts("Liftoff!");
+    return NULL;
+}
+
+int main(void){
+    pthread_t t1;
+    pthread_t t2;
+
+    count = 6;
+
+    pthread_create(&t1, NULL, run1, NULL);
+    pthread_create(&t2, NULL, run2, NULL);
+
+    while (count > 0){
+        lock();
+        count--;
+        printf("%d\n", count);
+        pthread_cond_broadcast(&condition);
+        unlock();
+        sleep(1);
+    }
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+}
